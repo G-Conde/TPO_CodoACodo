0f9b8cf5eccd86020d488fe538bab9e8f8535f8a
german <germanconde79@gmail.com>
2022-10-27 20:49:12 -0300

    ultimando detalles
    actualizacion

---
+++ contacto.js
@@ -0,0 +1,20 @@
+const $form = document.querySelector('#form')
+
+        $form.addEventListener('submit', handleSubmit)
+
+        async function handleSubmit(event){ 
+            event.preventDefault()
+            const form = new FormData(this)
+            const response = await fetch(this.action, {
+                method: this.method,
+                body: form,
+                headers:{
+                    'Accept': 'application/json'
+                }
+            } ) 
+            if (response.ok){
+                this.reset()
+               alert('Gracias por contactarnos, pronto te responderemos') 
+            }  
+        }
+  
