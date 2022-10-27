a54bca093f96e9404c5fc0c5c52c602ceeb01d0c
german <germanconde79@gmail.com>
2022-10-26 21:19:47 -0300

    adicionando algunos cambios
    se agrego la pag de registro/login
    
    Co-Authored-By: OmiwanKenobi <112966087+OmiwanKenobi@users.noreply.github.com>

---
+++ footer.js
@@ -0,0 +1,25 @@
+
+document.getElementById("footer").innerHTML=`
+<footer class="pie-pagina">
+<div class="grupo-1">
+    <div class="box">
+        <figure><img src="img/NBA-logo-png.png" alt="NBA"></figure>
+    </div>
+        <div class="box">
+        <H2>DONDE NOS ENCONTRAMOS</H2>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1381.0763570751092!2d-58.42941385294552!3d-34.58464382043537!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb5f06aae67a3%3A0x7f4a12170a77fc2e!2sVite%20Porcellanato%20Showroom!5e0!3m2!1ses-419!2sar!4v1662506401987!5m2!1ses-419!2sar" width="200" height="100" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+            <div class="box">
+            <h2>SIGUENOS</h2>
+                <div class="red-social">
+                    <a href="#" class="fa fa-facebook"></a>
+                    <a href="#" class="fa fa-instagram"></a>
+                    <a href="#" class="fa fa-twitter"></a>
+                    <a href="#" class="fa fa-youtube"></a>
+                </div>
+            </div>
+</div>
+                <div class="grupo-2">
+                <small>&copy; 2022 <strong>NBA Store | Argentina</strong>-Todos los derechos reservados</small>
+                </div>
+</footer>`
